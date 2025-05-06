9a2b700daebf39781608b556c752cea2e0d2ceaa
soumadeepsengupta <154908784+soumadeepsengupta@users.noreply.github.com>
2025-05-06 21:39:55 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="ticker-container bg-yellow-300 h-10 fixed bottom-0 w-full">
+      <div className="animate-marquee text-[#2D0100] font-medium h-full flex justify-center items-center">
+        START YOUR ADOPTION JOURNEY TODAY!
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
